a50787fd2073886f06fa085818e7595f9e03f2e6
nissafors <aa.gpost@gmail.com>
2016-02-02 13:06:53 +0100

    Initial commit

---
+++ Bibblan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bibblan.Startup))]
+namespace Bibblan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
