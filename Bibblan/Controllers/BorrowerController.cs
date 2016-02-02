c87478cedc12a7538029e09ebe2f76f0fa17359c
Henrik Ottehall <othe1492@student.ju.se>
2016-02-02 15:59:08 +0100

    Adds initial page for borrowers loans.

---
+++ Bibblan/Controllers/BorrowerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bibblan.Models;
+
+namespace Bibblan.Controllers
+{
+   public class BorrowerController : Controller
+   {
+      //
+      // GET: /Borrower/
+      public ActionResult Mypage(int ISBN = 0)
+      {
+         return View();
+      }
+   }
+}
