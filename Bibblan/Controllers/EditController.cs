4bf973ba8f28b39c7d4839a7072a843f5f45a038
nissafors <aa.gpost@gmail.com>
2016-02-15 17:28:18 +0100

    Edit book

--- Bibblan/Controllers/EditController.cs
+++ Bibblan/Controllers/EditController.cs
@@ -16,20 +16,26 @@ namespace Bibblan.Controllers
             return View();
         }
 
-        public ActionResult Book()
+        public ActionResult Book(Book book = null)
         {
+            // Debug
+            //book = Mockup.books[2];
+
+            IEnumerable<BookAuthor> bookAuthors = from ba in Mockup.bookAuthors where ba.Book == book select ba;
+            int authorId = bookAuthors.Count() > 0 ? bookAuthors.ElementAt(0).Author.Id : 0;
+
             SelectList authors = new SelectList((from s in Mockup.authors
                                                 select new
                                                 {
                                                     Id = s.Id,
                                                     FullName = s.FirstName + " " + s.LastName
-                                                }), "Id", "FullName");
+                                                }), "Id", "FullName", authorId);
             ViewData["author"] = authors;
 
-            SelectList classifications = new SelectList(Mockup.classifications, "Id", "Signum");
+            IEnumerable<SelectListItem> classifications = new SelectList(Mockup.classifications, "Id", "Signum");
             ViewData["classification"] = classifications;
 
-            return View();
+            return View(book);
         }
 
         public ActionResult Borrower()
