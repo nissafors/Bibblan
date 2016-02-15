7fafef48d3e011c5ab42e5bcdf15d0bb7327f732
johnssonelias <johnssonelias@hotmail.com>
2016-02-15 13:12:43 +0100

    Added  BorrowerPages

--- Bibblan/Controllers/EditController.cs
+++ Bibblan/Controllers/EditController.cs
@@ -13,5 +13,10 @@ namespace Bibblan.Controllers
         {
             return View();
         }
+
+        public ActionResult Borrower()
+        {
+            return View();
+        }
     }
 }
