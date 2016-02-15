f054814791f20ec820a81c9f1069c1e29454cf67
Henrik Ottehall <othe1492@student.ju.se>
2016-02-15 12:10:22 +0100

    Adds Author and Book search

--- Bibblan/Controllers/SearchController.cs
+++ Bibblan/Controllers/SearchController.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Common.Models;
 
 namespace Bibblan.Controllers
 {
     public class SearchController : Controller
     {
+        static public List<Classification> cList = new List<Classification>
+            {
+                new Classification{ Description="Test1", Id=1, Signum="Test1"},
+                new Classification{ Description="Test2", Id=2, Signum="Test2"},
+                new Classification{ Description="Test3", Id=3, Signum="Test3"}
+            };
+
+
         //
         // GET: /Search/
         public ActionResult Results()
         {
+            return View();
+        }
+
+        //
+        // GET: /Search/
+        public ActionResult Author()
+        {
+            return View();
+        }
+
+        public ActionResult Book()
+        {
+            ViewData["cList"] = cList;
             return View();
         }
 	}
