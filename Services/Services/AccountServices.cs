8265a5098026162f3b5e1036bc0e9faeda55f039
johnssonelias <johnssonelias@hotmail.com>
2016-02-16 13:17:47 +0100

    Add AccountServices & AuthorServices

---
+++ Services/Services/AccountServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Models;
+
+namespace Services.Services
+{
+    public class AccountServices
+    {
+        // TODO LOGIN LOGOUT
+        /* 
+        public Borrower Login()
+        {
+
+        }
+        */
+        public List<Loan> GetLoans(Borrower user)
+        {
+            return user.Loans;
+        }
+    }
+}
