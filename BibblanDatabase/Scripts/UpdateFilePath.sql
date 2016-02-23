1e97b96f3f67b20ddd43386f87f26d3626cc018f
Henrik Ottehall <othe1492@student.ju.se>
2016-02-23 14:58:01 +0100

    Adds UpdateFilePath for DB

---
+++ BibblanDatabase/Scripts/UpdateFilePath.sql
@@ -0,0 +1,13 @@
+﻿ALTER DATABASE BibblanDatabase SET OFFLINE
+GO
+
+ALTER DATABASE BibblanDatabase MODIFY FILE (NAME = BibblanDatabase, FILENAME = 'C:\Users\othe1492\Source\Repos\Bibblan\BibblanDatabase\Database\BibblanDatabase.mdf');
+ALTER DATABASE BibblanDatabase MODIFY FILE (NAME = BibblanDatabase_log, FILENAME = 'C:\Users\othe1492\Source\Repos\Bibblan\BibblanDatabase\Database\BibblanDatabase_log.ldf');
+GO
+
+ALTER DATABASE BibblanDatabase SET ONLINE
+GO
+
+--SELECT name, physical_name AS CurrentLocation, state_desc
+--FROM sys.master_files
+--WHERE database_id = DB_ID(N'BibblanDatabase');
