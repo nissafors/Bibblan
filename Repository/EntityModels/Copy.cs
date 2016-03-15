ef37befef9a27bbbedba1e01b6ef6c09f0722378
nissafors <aa.gpost@gmail.com>
2016-03-15 15:49:06 +0100

    Documentation in Repository/EntityModels/Copy

--- Repository/EntityModels/Copy.cs
+++ Repository/EntityModels/Copy.cs
@@ -16,6 +16,13 @@ namespace Repository.EntityModels
         public string ISBN { get; set; }
         public string Library { get; set; }
 
+        /// <summary>
+        /// Get all Copy:s matching the given ISBN.
+        /// </summary>
+        /// <param name="copies">A List of Copy:s. Will be set to a new instance.
+        /// May be empty after execution if no records were found.</param>
+        /// <param name="isbn">The ISBN as a string.</param>
+        /// <returns>Returns true if no error occured.</returns>
         public static bool GetCopies(out List<Copy> copies, string isbn)
         {
             SqlCommand command = new SqlCommand("SELECT * from COPY WHERE ISBN = @ISBN");
@@ -23,16 +30,28 @@ namespace Repository.EntityModels
             return getCopies(out copies, command);
         }
 
-        public static bool GetCopy(out Copy copy, string barCode)
+        /// <summary>
+        /// Fetch a Copy from the database.
+        /// </summary>
+        /// <param name="copy">A variable of type Copy. Will be set to a new instance if a record was found.
+        /// Will be null after execution if no record was found.</param>
+        /// <param name="barcode">The Barcode of the Copy to fetch.</param>
+        /// <returns>Returns true if no error occured.</returns>
+        public static bool GetCopy(out Copy copy, string barcode)
         {
             SqlCommand command = new SqlCommand("SELECT * from COPY WHERE Barcode = @Barcode");
-            command.Parameters.AddWithValue("@Barcode", barCode);
+            command.Parameters.AddWithValue("@Barcode", barcode);
             var copies = new List<Copy>();
             var result = getCopies(out copies, command);
             copy = result && copies.Count > 0 ? copies[0] : null;
             return result;
         }
 
+        /// <summary>
+        /// Update a Copy or create a Copy if it doesn't exist. This means existing items will be overwritten.
+        /// </summary>
+        /// <param name="copy">The Copy to write to the database.</param>
+        /// <returns>Returns true if one row was written and no error occured.</returns>
         static public bool Upsert(Copy copy)
         {
             try
@@ -65,6 +84,11 @@ namespace Repository.EntityModels
             return true;
         }
 
+        /// <summary>
+        /// Delete a record from the database.
+        /// </summary>
+        /// <param name="barcode">The Barcode of the Copy to delete.</param>
+        /// <returns>Returns true if no error occured.</returns>
         public static bool Delete(string barcode)
         {
             try
@@ -89,6 +113,13 @@ namespace Repository.EntityModels
             return true;
         }
 
+        /// <summary>
+        /// Exectute command and place records from the database in a list.
+        /// </summary>
+        /// <param name="copies">A List of Copy:s. Will be set to a new instance.
+        /// May be empty after execution if no records were found.</param>
+        /// <param name="command">The SqlCommand to execute.</param>
+        /// <returns>Returns true of no errors occured.</returns>
         private static bool getCopies(out List<Copy> copies, SqlCommand command)
         {
             copies = new List<Copy>();
