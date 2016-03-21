f632ca73970f5baf889b34eb8002f400070b6b9a
elias <elias>
2016-03-21 11:52:06 +0100

    Refactor Entities & Services, Add Fields on AccountVm

--- Common/Models/AccountViewModels.cs
+++ Common/Models/AccountViewModels.cs
@@ -10,6 +10,8 @@ namespace Common.Models
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public string NewPassword { get; set; }
+        public string NewPasswordAgain { get; set; }
         public int RoleId { get; set; }
         public string PersonId { get; set; }
     }
