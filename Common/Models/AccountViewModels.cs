2a171fedefd23970f3f5bbe05a64d7aae413dc88
Henrik Ottehall <othe1492@student.ju.se>
2016-03-21 12:26:30 +0100

    Fixes password validation when editing borrower

--- Common/Models/AccountViewModels.cs
+++ Common/Models/AccountViewModels.cs
@@ -11,11 +11,23 @@ namespace Common.Models
 
     public class AccountViewModel
     {
+        [Display(Name = "Användarnamn")]
         public string Username { get; set; }
+        
+        [Display(Name = "Lösenord")]
         public string Password { get; set; }
+        
+        [Display(Name = "Lösenord")]
         public string NewPassword { get; set; }
+
+        // System.ComponentModel.DataAnnotations.CompareAttribute should be used instead of
+        // System.Web.Mvc.Compare but it contains known bug in our version
+        [Display(Name = "Lösenord igen")]
+        [System.Web.Mvc.Compare("NewPassword", ErrorMessage="Lösenorden är inte lika.")]
         public string NewPasswordAgain { get; set; }
+        
         public int RoleId { get; set; }
+        
         public string PersonId { get; set; }
     }
 
