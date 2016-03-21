88544ae208862122b1dba5b527df5ed05eb96ea7
nissafors <aa.gpost@gmail.com>
2016-03-21 10:45:10 +0100

    Revert "Removed unused files."
    This reverts commit 720f1c1ec1efe6fccfbd14a54823fb81de7e31be.

---
+++ Common/Models/ResultViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Common.Models
+{
+    // Not used at all?
+    public class ResultViewModel
+    {
+        public List<BookViewModel> Books { get; set; }
+    }
+}
